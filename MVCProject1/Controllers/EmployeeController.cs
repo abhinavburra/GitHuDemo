5053105ceba0602d5d85aec482c446add0d350eb
abhinavburra <64180259+abhinavburra@users.noreply.github.com>
2022-07-23 19:34:38 +0530

    new

---
+++ MVCProject1/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject1.Models;
+
+
+namespace MVCProject1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+            Employee employee=new Employee()
+            {
+                EmployeeId = 1001,
+                EmployeeName = "Abhinav",
+                EmployeeLocation = "Hyderabad"
+            };
+            return View(employee);
+        }
+    }
+}
---
+++ MVCProject1/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject1.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeLocation { get; set; }
+
+    }
+}
